4d3d8e98002fe8b4085e1cfe48e6ff2b3abb5218
Maxim Vorchakov <m.vorchakov@bk.ru>
2019-01-02 15:59:27 +0200

    Added AudioUpload & AudioItem components

---
+++ src/components/AudioItem/AudioItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './AudioItem.css';
+
+const AudioItem = (props) => {
+    return (
+        <div className="container">
+            <div className="row" >
+                <div className="col-xs-11" >
+                    <button className="btn-audio-item-delete" >
+                        <i className="fas fa-times" ></i>
+                    </button></div>
+                <div className="col-xs-11 audio-item" >
+                    <div className="col-xs-4" >
+                        <label>№{props.id} {props.name}</label>
+                    </div>
+                    <div className="col-xs-8" >
+                        <textarea className="audio-text" placeholder="Введіть оригінал тексту до аудіо."></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AudioItem;
---
+++ src/components/AudioUpload/AudioUpload.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import AudioItem from "../AudioItem/AudioItem";
+import './AudioUpload.css';
+
+class AudioUpload extends Component {
+  state = {
+      filesToBeUpload: [] // Variable for storing audio files
+  }
+
+  fileChangedHandler = event => {
+      // Displaing all selected files
+      let audioContainer = document.getElementById("audio-container")
+
+      for (let i = 0; i < event.target.files.length; i++) {
+        // Creating unique div elements to avoid replacing them by ReactDOM.render
+        const id = i + 1
+        const d = document.createElement("div")
+        d.id = id
+        audioContainer.appendChild(d)
+
+        // Rendering each selected audio item
+        ReactDOM.render(<AudioItem name={event.target.files[i].name} id={id} />, document.getElementById(id))
+      }
+
+      // Saving files to 'filesToBeUpload' state
+      this.setState({
+        filesToBeUpload: event.target.files
+      });
+  }
+
+  fileUploadHandler = (event) => {
+    // Do we need hard code URL? 
+    const apiBaseUrl = "https://localhost:44323/api/upload";
+
+    // Checking if file array is empty
+    if (this.state.filesToBeUpload.length > 0) {
+
+      //Geting text from textarea
+      const textToAudioArray = document.getElementsByClassName('audio-text'); 
+
+      let filesArray = this.state.filesToBeUpload;
+
+      let formData = new FormData();
+
+      for (var i = 0; i < filesArray.length; i++) {
+        // Adding data to formData element
+        formData = new FormData();
+        formData.append("AudioFile", filesArray[i]);
+        formData.append("Text", textToAudioArray[i].value);
+
+        // Sending post requset to server
+        axios.post(apiBaseUrl, formData, {
+          headers: { 'Content-Type': 'multipart/form-data' }
+        });
+      }
+    }
+    else {
+      alert("Select audio files");
+    }
+  }
+
+  render() {
+    return (
+      <div className="audio-upload-section">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12 audio-header">
+              <label>Аудіо</label>
+
+              <input
+                style={{ display: 'none' }}
+                type="file"
+                accept="audio/*"
+                onChange={this.fileChangedHandler}
+                ref={fileInput => this.fileInput = fileInput}
+                multiple />
+
+              <button id="btn-audio-add" onClick={() => this.fileInput.click()}><i className="fas fa-plus-circle"></i></button>
+              <button id="btn-audio-upload" onClick={this.fileUploadHandler}><i className="fas fa-upload"></i> Завантажити</button>
+            </div>
+          </div>
+        </div>
+        
+        {/* Files to be upload list */}
+        <div className="container">
+          <div id="audio-container" className="row">
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AudioUpload;
