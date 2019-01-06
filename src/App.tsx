ad2b00a6760af37604dabdbcc639efdd41179c71
Dmitry Martynenko <d0961477767@gmail.com>
2019-01-07 00:44:32 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-class App extends Component {
+import PerformanceList from "./components/Performances/PerformanceList"
+class PerformancesPage extends Component {
   render() {
     return (
       <div className="App">
-   App
+        <PerformanceList />
       </div>
     );
   }
 }
 
-export default App;
+export default PerformancesPage;
