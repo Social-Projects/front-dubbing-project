0da2a7837253a758a87116581b96ec3d58739534
Andrei Kasyanchuk <kasanandry@gmail.com>
2019-01-15 17:32:29 +0200

    Transfered func getAudioInfo to apiManager.ts and deleted ApiManagerStreamingPage

--- src/apiManager.ts
+++ src/apiManager.ts
@@ -64,5 +64,20 @@ class apiManager
             }});
             return response;
     }
+
+
+    //Using for display list of audio on stream page
+    async getAudioInfo(index:number): Promise<Response> {
+        const response = await fetch(`${this.backendUrl}api/Perfomance/${index}/AudiosInfo`,
+            {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            });
+        return response;
+    }
 }
 export default apiManager;
--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -3,7 +3,7 @@ import StreamHead from './StreamHead/StreamHead';
 import Aux from '../../hoc/Auxiliary';
 import WithClass from '../../hoc/WithClass';
 import classes from './Stream.module.css';
-import apiManager from './ApiManagerStreamingPage';
+import apiManager from '../../apiManager';
 import { async } from 'q';
 
 interface streamState{
@@ -37,6 +37,7 @@ class Stream extends Component<streamProps, streamState> {
                     audioInfo: data
                 });
     }
+
 
     render() {
         return (
