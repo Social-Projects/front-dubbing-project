f1a6ce764cf16e3da89fc9f08dd5ffd5d63aaa49
Andrei Kasyanchuk <kasanandry@gmail.com>
2019-01-18 14:34:03 +0200

    Added performance id

--- src/components/Stream/Stream.tsx
+++ src/components/Stream/Stream.tsx
@@ -33,7 +33,7 @@ class Stream extends Component<streamProps, streamState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            perfomanceId: -1,
+            perfomanceId: this.props.match.params.number,
             isPlay: false,
             audioInfo: [],
             currentSpeechId: -1
@@ -73,12 +73,13 @@ class Stream extends Component<streamProps, streamState> {
     }
 
     async componentDidMount() {
-        this.setState({
-            perfomanceId: this.props.match.params.number
-        });
+        // this.setState({
+        //     perfomanceId: parseInt(this.props.match.params.number.toString())
+        // });
+        console.log("state: " + this.state.perfomanceId);
 
-        await this.apiManager.load(1);
-        await this.getSpeechInfo(1);
+        await this.apiManager.load(this.state.perfomanceId);
+        await this.getSpeechInfo(this.state.perfomanceId);
     }
     
     render() {
